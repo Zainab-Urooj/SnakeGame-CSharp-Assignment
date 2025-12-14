24275449c85a7440801bd221f78796a942c51bf4
Zainab Urooj <uroojzainab088@gmail.com>
2025-12-14 12:16:33 +0500

    Add files via upload

---
+++ Solution File/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlMenu = new Panel();
+            btnGameStart = new Button();
+            label2 = new Label();
+            btnDifficult = new Button();
+            btnMedium = new Button();
+            btnEasy = new Button();
+            label1 = new Label();
+            pnlMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            pnlMenu.BackColor = Color.ForestGreen;
+            pnlMenu.Controls.Add(btnGameStart);
+            pnlMenu.Controls.Add(label2);
+            pnlMenu.Controls.Add(btnDifficult);
+            pnlMenu.Controls.Add(btnMedium);
+            pnlMenu.Controls.Add(btnEasy);
+            pnlMenu.Controls.Add(label1);
+            pnlMenu.ForeColor = Color.FromArgb(255, 255, 128);
+            pnlMenu.Location = new Point(0, 0);
+            pnlMenu.Name = "pnlMenu";
+            pnlMenu.Size = new Size(878, 544);
+            pnlMenu.TabIndex = 0;
+            // 
+            // btnGameStart
+            // 
+            btnGameStart.BackColor = Color.FromArgb(255, 255, 128);
+            btnGameStart.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGameStart.ForeColor = Color.ForestGreen;
+            btnGameStart.Location = new Point(336, 450);
+            btnGameStart.Name = "btnGameStart";
+            btnGameStart.Size = new Size(177, 44);
+            btnGameStart.TabIndex = 6;
+            btnGameStart.Text = "Game Start";
+            btnGameStart.UseVisualStyleBackColor = false;
+            btnGameStart.Click += btnGameStart_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(322, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(209, 28);
+            label2.TabIndex = 5;
+            label2.Text = "Choose Difficulty Level";
+            // 
+            // btnDifficult
+            // 
+            btnDifficult.BackColor = Color.ForestGreen;
+            btnDifficult.Font = new Font("Snap ITC", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDifficult.Location = new Point(295, 350);
+            btnDifficult.Name = "btnDifficult";
+            btnDifficult.Size = new Size(264, 60);
+            btnDifficult.TabIndex = 4;
+            btnDifficult.Text = "Difficult";
+            btnDifficult.UseVisualStyleBackColor = false;
+            btnDifficult.Click += btnDifficult_Click;
+            
+            // 
+            // btnMedium
+            // 
+            btnMedium.BackColor = Color.ForestGreen;
+            btnMedium.Font = new Font("Snap ITC", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMedium.Location = new Point(295, 270);
+            btnMedium.Name = "btnMedium";
+            btnMedium.Size = new Size(261, 57);
+            btnMedium.TabIndex = 3;
+            btnMedium.Text = "Medium";
+            btnMedium.UseVisualStyleBackColor = false;
+            btnMedium.Click += btnMedium_Click;
+           
+            // 
+            // btnEasy
+            // 
+            btnEasy.BackColor = Color.ForestGreen;
+            btnEasy.Font = new Font("Snap ITC", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEasy.ForeColor = Color.FromArgb(255, 255, 128);
+            btnEasy.Location = new Point(295, 187);
+            btnEasy.Name = "btnEasy";
+            btnEasy.Size = new Size(261, 58);
+            btnEasy.TabIndex = 2;
+            btnEasy.Text = "Easy";
+            btnEasy.UseVisualStyleBackColor = false;
+            btnEasy.Click += btnEasy_Click;
+           
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(226, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(419, 73);
+            label1.TabIndex = 1;
+            label1.Text = "Snake Game";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.ForestGreen;
+            ClientSize = new Size(878, 544);
+            Controls.Add(pnlMenu);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "snake";
+            pnlMenu.ResumeLayout(false);
+            pnlMenu.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlMenu;
+        private Label label1;
+        private Button btnEasy;
+        private Button btnMedium;
+        private Label label2;
+        private Button btnDifficult;
+        private Button btnGameStart;
+    }
+}
---
+++ Solution File/loading_screen.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SnakeGame
+{
+    partial class loading_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loading_screen));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(243, 71);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(379, 354);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(216, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(419, 73);
+            label1.TabIndex = 1;
+            label1.Text = "Snake Game";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(progressBar1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(878, 544);
+            panel1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            progressBar1.BackColor = Color.FromArgb(255, 255, 128);
+            progressBar1.ForeColor = Color.FromArgb(255, 255, 128);
+            progressBar1.Location = new Point(285, 487);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(300, 25);
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 20;
+            timer1.Tick += timer1_Tick;
+            // 
+            // loading_screen
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.ForestGreen;
+            ClientSize = new Size(878, 544);
+            Controls.Add(panel1);
+            Name = "loading_screen";
+            Text = "loading_screen";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private ProgressBar progressBar1;
+    }
+}
